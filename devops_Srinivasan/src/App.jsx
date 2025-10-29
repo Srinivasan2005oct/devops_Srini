97ee429b25db36ebe009ec6a1092a2aba5a36474
Divyadharshini29042006 <krishnan.divya.rv@gmail.com>
2025-10-29 15:44:44 +0530

    first commit

--- devops_Srinivasan/src/App.jsx
+++ devops_Srinivasan/src/App.jsx
@@ -3,22 +3,27 @@ function App()
 {
   function showmessage()
   {
-    alert=("submitted");
+    alert={submitted};
+  }
+  function OTP()
+  {
+    alert="opt generated";
   }
 
   return (
 
     <div>
+      <p>2312127-Srinivasan S</p>
       <h1>Registration Form</h1>
       <h3>Fill your details to register</h3>
-      <input type="text"></input>Username <hr />
-      <input type="email"></input>Email <hr></hr>
-      <input type="password"></input>Password <hr />
-      <input type="password"></input> Confirm password <hr />
-        <input type="number"></input> Enter your Otp <hr />
+    Username<input type="text"></input> <hr />
+    Email<input type="email"></input> <hr></hr>
+    Password <input type="password"></input> <hr />
+    Confirm password <input type="password"></input>  <hr />
+    Enter your Otp <input type="number"></input>  <hr />
       <button onClick={showmessage}>Submit </button> <hr />
       
-      <button onClick={showmessage}>Generate otp</button>
+      <button onClick={OTP}>Generate otp</button>
     </div>
   )
 }
